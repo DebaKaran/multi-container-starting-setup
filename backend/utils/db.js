d98011dd5fced617f425eded5abffda6aaf5e9c7
DebaKaran <karan.deba@gmail.com>
2025-06-21 23:31:22 +0530

    refactor: move environment variables from Dockerfile to .env files, fix MongoDB auth issue
    - Moved APP_PORT and MongoDB credentials into
    .env and .env.backend
    - Removed ENV APP_PORT from backend Dockerfile
    - Updated docker-compose to load .env files for
    backend, seed, mongodb
    - Resolved "AuthenticationFailed" error caused by
     old MongoDB volume with invalid user
    - Documented the issue and resolution steps in
    README.md

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,9 +1,9 @@
 // backend/util/db.js
-require("dotenv").config();
+// require("dotenv").config(); not required
 
 const USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
 const PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
-const DATABASE = process.env.MONGODB_DATABASE;
+const DATABASE = process.env.MONGO_INITDB_DATABASE;
 const HOST = "mongodb"; // Docker service name
 
 const MONGODB_URL = `mongodb://${USERNAME}:${PASSWORD}@${HOST}:27017/${DATABASE}?authSource=admin`;
