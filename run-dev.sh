6e66066d037e6954b3be37e533b7df112820e235
DebaKaran <karan.deba@gmail.com>
2025-06-29 20:41:07 +0530

    Running frontend in detached mode

--- run-dev.sh
+++ run-dev.sh
@@ -3,4 +3,4 @@ echo "Running Dev Mode on port 3001 ..."
 docker-compose down
 export BUILD_TARGET=dev
 export PORT=3001
-docker-compose -f docker-compose.yaml -f docker-compose.override.yaml up --build
+docker-compose -f docker-compose.yaml -f docker-compose.override.yaml up -d --build
